fb341e303d4104cb558594112f6a21d674cc9d9e
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-01 21:37:20 +0530

    Time: 0 ms (100%), Space: 13.9 MB (8.88%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-    ListNode* getnode(ListNode* head , int idx){
-    ListNode* temp = head;
-        for(int i = 1;i<=idx;i++){
-            temp=temp->next;
-        }
-        return temp;
-    }
     ListNode* reverseList(ListNode* head) {
-    // method 1
-    // int n = 0;
-    // ListNode* temp = head;
-    // while(temp){
-    //     temp=temp->next;
-    //     n++;
-    // }
-    // int i =0; 
-    // int j = n-1;
-    // while(i<j){
-    //     ListNode* left = getnode(head,i);
-    //     ListNode* right = getnode(head,j);
-    //     int t =left->val;
-    //     left->val=right->val;
-    //     right->val=t;
-    //     i++;j--;
-    // }
-    //     return head;
+    stack<ListNode*>st;
+    ListNode* temp = head;
+    while(temp){
+        st.push(temp);
+        temp = temp->next;
+    }
+    if (st.empty()) return NULL;
+    ListNode* x = st.top();
+    st.pop();
+    temp = x;
+    while(st.size()>0){
 
-    //method  2  
-    // ListNode* prev=NULL;
-    // ListNode* curr=head;
-    // ListNode* NEXT=head;
-    // while(curr!=NULL){
-    //     NEXT=curr->next;
-    //     curr->next=prev;
-    //     prev=curr;
-    //     curr=NEXT;
-    // }
-    // return prev;
-    
-        //method 3
-        if(head==NULL || head->next==NULL) return head;
-        ListNode* newhead=reverseList(head->next);
-        head->next->next=head;
-        head->next=NULL;
-        return newhead;
+        temp->next = st.top();
+        st.pop();
+        temp = temp->next;
+    }
+    temp->next = NULL; 
+    return x;
     }
 };
