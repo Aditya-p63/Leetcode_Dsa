4e4d4819a15737e328b03d28684ed762d31a8fbe
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-03 00:10:02 +0530

    Time: 0 ms (100%), Space: 19.9 MB (8.99%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,58 +1,23 @@
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
-            //method 1
-
-        // ListNode* tempa=a;
-        // ListNode* tempb=b;
-        // ListNode* c = new ListNode(100);
-        // ListNode* tempc=c;
-        // while(tempa!=NULL && tempb!=NULL){
-        //     if(tempa->val <= tempb->val){
-        //         ListNode* t= new ListNode(tempa->val);
-        //         tempc->next=t;
-        //         tempc=t;
-        //         tempa=tempa->next;
-        //     }
-        //     else {
-        //         ListNode* t= new ListNode(tempb->val);
-        //         tempc->next=t;
-        //         tempc=t;
-        //         tempb=tempb->next;
-        //     }
-        // }
-        // if(tempa==NULL){
-        //     tempc->next=tempb;
-        // }
-        // else{
-        //     tempc->next=tempa;
-        // }
-        // return c->next;
-
-        //method 2
-
-        
-        ListNode* c = new ListNode(100);
-        ListNode* temp=c;
-        while(a!=NULL && b!=NULL){
-            if(a->val <= b->val){
-                c->next=a;
-                c=c->next;
-                a=a->next;
-            }
-            else {
-                c->next=b;
-                c=c->next;
-                b=b->next;
-            }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        vector<int>arr;
+        while(list1!=NULL){
+            arr.push_back(list1->val);
+            list1 = list1->next;
         }
-        if(a==NULL){
-            c->next=b;
+        while(list2!=NULL){
+            arr.push_back(list2->val);
+            list2 = list2->next;
         }
-        else{
-            c->next=a;
+        sort(arr.begin() , arr.end());
+        ListNode* n = new ListNode(-1);
+        ListNode* temp = n;
+        for(int i = 0; i < arr.size(); i++){
+            ListNode* t = new ListNode(arr[i]);
+            temp->next = t;
+            temp = temp->next;
         }
-        return temp->next;
-
+        return n->next;
     }
 };
