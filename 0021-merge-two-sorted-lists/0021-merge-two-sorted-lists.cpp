ceb92207650df0e33bf7bc9a79d218d74c6058d5
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-03 00:09:53 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -3,31 +3,56 @@ public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
             //method 1
 
-        ListNode* tempa=a;
-        ListNode* tempb=b;
+        // ListNode* tempa=a;
+        // ListNode* tempb=b;
+        // ListNode* c = new ListNode(100);
+        // ListNode* tempc=c;
+        // while(tempa!=NULL && tempb!=NULL){
+        //     if(tempa->val <= tempb->val){
+        //         ListNode* t= new ListNode(tempa->val);
+        //         tempc->next=t;
+        //         tempc=t;
+        //         tempa=tempa->next;
+        //     }
+        //     else {
+        //         ListNode* t= new ListNode(tempb->val);
+        //         tempc->next=t;
+        //         tempc=t;
+        //         tempb=tempb->next;
+        //     }
+        // }
+        // if(tempa==NULL){
+        //     tempc->next=tempb;
+        // }
+        // else{
+        //     tempc->next=tempa;
+        // }
+        // return c->next;
+
+        //method 2
+
+        
         ListNode* c = new ListNode(100);
-        ListNode* tempc=c;
-        while(tempa!=NULL && tempb!=NULL){
-            if(tempa->val <= tempb->val){
-                ListNode* t= new ListNode(tempa->val);
-                tempc->next=t;
-                tempc=t;
-                tempa=tempa->next;
+        ListNode* temp=c;
+        while(a!=NULL && b!=NULL){
+            if(a->val <= b->val){
+                c->next=a;
+                c=c->next;
+                a=a->next;
             }
             else {
-                ListNode* t= new ListNode(tempb->val);
-                tempc->next=t;
-                tempc=t;
-                tempb=tempb->next;
+                c->next=b;
+                c=c->next;
+                b=b->next;
             }
         }
-        if(tempa==NULL){
-            tempc->next=tempb;
+        if(a==NULL){
+            c->next=b;
         }
         else{
-            tempc->next=tempa;
+            c->next=a;
         }
-        return c->next;
+        return temp->next;
 
     }
 };
