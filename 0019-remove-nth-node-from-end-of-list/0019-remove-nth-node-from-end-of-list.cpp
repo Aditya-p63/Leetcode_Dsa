b266afdab77cc94f95f1e31b93db9c38ca02ca61
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-03 00:10:31 +0530

    Time: 0 ms (100%), Space: 14.8 MB (93.22%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len=0;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            len++;
-            temp=temp->next;
+        //method 1
+        // int len=0;
+        // ListNode* temp=head;
+        // while(temp!=NULL){
+        //     len++;
+        //     temp=temp->next;
+        // }
+        // if(n==len) {
+        //     head=head->next;
+        //     return head;
+        // }
+        // int m=len-n+1;
+        // int h=m-1;
+        // temp = head;
+        // for(int i=1;i<=h-1;i++){
+        //     temp=temp->next;
+        // }
+        // temp->next=temp->next->next;
+        // return head;
+
+        //method 2
+        ListNode* fast=head;
+        ListNode* slow=head;
+        for(int i=1;i<=n+1;i++){
+            if(fast==NULL) return head->next;
+            fast=fast->next;
         }
-        if(n==len) {
-            head=head->next;
-            return head;
+        while(fast!=NULL){
+            slow=slow->next;
+            fast=fast->next;
         }
-        int m=len-n+1;
-        int h=m-1;
-        temp = head;
-        for(int i=1;i<=h-1;i++){
-            temp=temp->next;
-        }
-        temp->next=temp->next->next;
+        slow->next=slow->next->next;
         return head;
     }
 };
