badeb86df195132329c8f7ce82911cb77f939246
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-04 00:09:36 +0530

    Time: 3 ms (34.49%), Space: 77.1 MB (74.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* n = new ListNode(-1);
-        ListNode* tempn = n;
-        int a = 0;
-        while(l1 != NULL && l2 != NULL){
-            a = a + l1->val;
-            int b = l2->val;
-            int c = a+b;
-            int d = c%10;
-            c = c/10;
-            int e = c%10;
-            ListNode* temp = new ListNode(d);
-            tempn->next = temp;
-            tempn = tempn->next;
-            if(e!=0) a = e;
-            else a = 0;
-            l1 = l1->next;
-            l2 = l2->next;
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
+        int carry = 0;
+        while ((l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0;
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
+            temp->next = node;
+            temp = temp->next;
         }
-        while(l1!=NULL){
-            a = a +  l1->val;
-            int b = a%10;
-            a = a/10;
-            int c = a%10;
-            ListNode* temp = new ListNode(b);
-            tempn->next = temp;
-            tempn = tempn->next;
-            if(c!=0) a = c;
-            else a = 0;
-            l1 = l1->next;
-        }
-        while(l2!=NULL){
-            a = a + l2->val;
-            int b = a%10;
-            a = a/10;
-            int c = a%10;
-            ListNode* temp = new ListNode(b);
-            tempn->next = temp;
-            tempn = tempn->next;
-            if(c!=0) a = c;
-            else a = 0;
-            l2 = l2->next;
-        }
-        if(a!=NULL) {
-            ListNode* n = new ListNode(a);
-            tempn->next = n;
-        }
-        return n->next;
+        return dummy->next;
     }
 };
