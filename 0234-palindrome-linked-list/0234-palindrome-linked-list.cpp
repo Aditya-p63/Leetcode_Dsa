df63cb0a896ecd01fc56c5a68e385b717ea3afcf
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-05 00:10:32 +0530

    Time: 15 ms (6.63%), Space: 132.7 MB (5.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,57 +1,18 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* NEXT = head;
-        while (curr != NULL) {
-            NEXT = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = NEXT;
-        }
-        return prev;
-    }
-
     bool isPalindrome(ListNode* head) {
-        //method 2
-        // ListNode* c = new ListNode(10);
-        // ListNode* temp = head;
-        // ListNode* tempc = c;
-        // while (temp) {
-        //     ListNode* node = new ListNode(temp->val);
-        //     tempc->next = node;
-        //     temp = temp->next;
-        //     tempc = tempc->next;
-        // }
-        // c = c->next;
-        // c=reverse(c);
-        // ListNode* a =head;
-        // ListNode* b =c;
-        // while(a){
-        //     if(a->val!=b->val)  return false;
-        //     a=a->next;
-        //     b=b->next;
-        // }
-        // return true;
-
-        //method 3
-
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+        stack<ListNode*>st;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            st.push(temp);
+            temp = temp->next;
         }
-        ListNode* right = reverse(slow->next);
-        ListNode* a = head;
-        ListNode* b = right;
-        while(b){
-            if(a->val!=b->val) return false;
-            a=a->next;
-            b=b->next;
+        temp = head;
+        while(temp!=NULL){
+            if(temp->val != st.top()->val) return false;
+            st.pop();
+            temp = temp->next;
         }
         return true;
-
     }
 };
