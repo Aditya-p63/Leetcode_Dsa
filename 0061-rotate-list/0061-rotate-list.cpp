da30fd2546d201d852c94c91523a7a202114b804
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-06 00:18:40 +0530

    Time: 3 ms (3.69%), Space: 16.4 MB (63.03%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,40 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    int getSize(ListNode* head) {
+        int cnt = 0;
+        while (head != NULL) {
+            cnt++;
+            head = head->next;
+        }
+        return cnt;
+    }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head -> next == NULL){
-            return head;
-        }
-        int n =0;
-        ListNode* tail = head;
+        // if(head == NULL || head->next == NULL || k==0) return head;
+        int size = getSize(head);
+        if(k==0 || size==0) return head;
+        k = k%size;
+        size = size-k-1;
         ListNode* temp = head;
-        while(temp!=NULL){
-            if(temp-> next == NULL){
-                tail = temp;
-            }
+        if(head == NULL || head->next == NULL || k==0) return head;
+        while(size > 0){
             temp = temp->next;
-            n++;
-        }
-        k = k%n;
-        if(k==0)
-            return head;
-        k = k%n;
-        temp = head;
-        for(int i =1; i<n-k; i++){
+            size--;
+        } 
+        ListNode* t = temp->next;
+        temp->next = nullptr;
+        temp = t;
+        while(temp->next != nullptr){
             temp = temp->next;
         }
-        tail-> next = head;
-        head = temp->next;
-        temp ->next =NULL;
-        return head;
+        temp->next = head;
+        return t;
     }
 };
