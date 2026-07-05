39e4b40e459ed267fed1d8cb49c8bd11dbd81fbd
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-06 00:19:36 +0530

    Time: 7 ms (51.68%), Space: 15.2 MB (47.19%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* dummy = new Node(10);
-        Node* tc = dummy;
+        if (head == nullptr) return nullptr;
+
+        unordered_map<Node*, Node*> mp;
+
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+
         Node* temp = head;
         while (temp) {
-            Node* a = new Node(temp->val);
-            tc->next = a;
-            tc = tc->next;
+            Node* copy = new Node(temp->val);
+
+            mp[temp] = copy;
+            tail->next = copy;
+            tail = copy;
+
             temp = temp->next;
         }
-        // step 2
-        Node* b = dummy->next;
-        Node* a = head;
-        unordered_map<Node* , Node* >m;
-        Node* tempa = a;
-        Node* tempb = b;
-        while(tempa){
-            m[tempa]=tempb;
-            tempa = tempa->next;
-            tempb = tempb->next;
+
+        temp = head;
+        while (temp) {
+            if (temp->random)
+                mp[temp]->random = mp[temp->random];
+
+            temp = temp->next;
         }
-        for(auto x : m){
-            Node* o = x.first;
-            Node* d = x.second;
-            if(o->random!=NULL){
-                Node* oran = o->random;
-                Node* dran = m[o->random];
-                d->random =  dran; 
-            }
-        }
-        return b;
+
+        return dummy->next;
     }
 };
