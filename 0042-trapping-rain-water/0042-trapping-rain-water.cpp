187f9148ef529ff907e40732e0bdb6135d4521bb
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-07 00:26:23 +0530

    Time: 0 ms (100%), Space: 26 MB (78.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -12,25 +12,19 @@ public:
                 max = height[i];
             }
         }
-        int next[n];
-        next[n - 1] = -1;
+        
+        prev[n - 1] = -1;
         max = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            next[i] = max;
+          if(max<prev[i])  prev[i] = max;
             if (max < height[i]) {
                 max = height[i];
             }
         }
-        // new arr for minumum
-        int mini[n];
-        for (int i = 0; i < n; i++) {
-            mini[i] = min(prev[i], next[i]);
-        }
-        // calculating water
         int water = 0;
         for (int i = 1; i < n - 1; i++) {
-            if (height[i] < mini[i]) {
-                water += (mini[i] - height[i]);
+            if (height[i] < prev[i]) {
+                water += (prev[i] - height[i]);
             }
         }
         return water;
