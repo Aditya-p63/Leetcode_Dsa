ca5fe8606753edb41d43ce08c710b41c8b7864e8
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-08 00:05:36 +0530

    Time: 0 ms (100%), Space: 13.9 MB (84.3%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-vector<vector<int>> ans;
-    void f(vector<int>& arr , int t , int idx , vector<int>& sub){
-        if(t==0){
-            ans.push_back(sub);
-            return ;
+    void f(int idx, vector<int>& arr, int target,
+           vector<int>& ds, vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
         }
-        if(idx==arr.size()) return;
-        if(arr[idx] <= t){
-            sub.push_back(arr[idx]);
-            f(arr,t-arr[idx],idx+1,sub);
-            sub.pop_back();
+        for (int i = idx; i < arr.size(); i++) {
+            if (i > idx && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+            ds.push_back(arr[i]);
+            f(i + 1, arr, target - arr[i], ds, ans);
+            ds.pop_back();
         }
-        int j = idx+1;
-        while(j < arr.size() and arr[j]==arr[j-1]) j++;
-            f(arr,t,j,sub);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-        sort(arr.begin(),arr.end());
-        vector<int>a;
-        ans.clear();
-        f(arr,target,0,a);
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        f(0, arr, target, ds, ans);
         return ans;
     }
 };
