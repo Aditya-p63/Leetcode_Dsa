e66c33e4b4344ed458883af303e8783b43ca44f2
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-09 00:15:16 +0530

    Time: 4 ms (33.29%), Space: 12.4 MB (14.29%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-    vector<vector<char>>grid;
-    vector<vector<string>>ans;
-    bool canplaceq(int r , int col , int n){
-        for(int i = r-1; i >= 0; i--){
-            if(grid[i][col]=='Q'){
+    bool isSafe(int row, int col, vector<vector<char>>& board, int n) {
+        for (int j = 0; j < col; j++) {
+            if (board[row][j] == 'Q')
                 return false;
-            }
         }
-        for(int i = r-1 , j = col - 1; i >= 0 and j >= 0 ; i--,j--){
-            if(grid[i][j]=='Q') return false;
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
         }
-
-        for(int i = r-1 , j = col + 1; i >= 0 and j < n; i--,j++){
-            if(grid[i][j]=='Q') return false;
+        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q')
+                return false;
         }
         return true;
     }
-    void f(int r , int n){
-        if(r==n){
-            vector<string>temp;
-            for(int i = 0; i < n; i++){
-                string s = "";
-                for(int j = 0; j < n; j++){
-                    s+=grid[i][j];
-                }
-                temp.push_back(s);
+    void solve(int col, vector<vector<char>>& board,
+               vector<vector<string>>& ans, int n) {
+        if (col == n) {
+            vector<string> temp;
+            for (int i = 0; i < n; i++) {
+                string row(board[i].begin(), board[i].end());
+                temp.push_back(row);
             }
             ans.push_back(temp);
             return;
         }
-        for(int col = 0; col < n; col++){
-            if(canplaceq(r,col,n)){
-                grid[r][col]='Q';
-                f(r+1,n);
-                grid[r][col]='.';
-            }
+        for (int row = 0; row < n; row++) {
 
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solve(col + 1, board, ans, n);
+                board[row][col] = '.';
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        grid.clear();
-        ans.clear();
-        grid.resize(n,vector<char>(n,'.'));
-        f(0,n);
+        vector<vector<string>> ans;
+
+        vector<vector<char>> board(n, vector<char>(n, '.'));
+        solve(0, board, ans, n);
         return ans;
     }
 };
