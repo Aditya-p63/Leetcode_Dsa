bb08ed92b001b1541414224400d770fae6121616
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-11 08:51:37 +0530

    Time: 0 ms (100%), Space: 10.1 MB (66.13%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,37 +1,53 @@
 class MyQueue {
 public:
-    stack<int> st;
-    stack<int> help;
-    MyQueue() {}
-
-    void push(int x) {
-        if (st.size() == 0) {
-            st.push(x);
-            return;
-        } else {
-            while (st.size() > 0) {
-                help.push(st.top());
-                st.pop();
-            }
-            st.push(x);
-            while (help.size() > 0) {
-                st.push(help.top());
-                help.pop();
-            }
-        }
+    stack<int>st1;
+    stack<int>st2;
+    MyQueue() {
+        
     }
-
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
     int pop() {
-        int x = st.top();
-        st.pop();
+         while(st1.size() > 0){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int x = st2.top();
+        st2.pop();
+         while(st2.size() > 0){
+            st1.push(st2.top());
+            st2.pop();
+        }
         return x;
     }
-
-    int peek() { return st.top(); }
-
+    
+    int peek() {
+        while(st1.size() > 0){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        int x = st2.top();
+         while(st2.size() > 0){
+            st1.push(st2.top());
+            st2.pop();
+        }
+        return x;
+    }
+    
     bool empty() {
-        if (st.size() == 0)
-            return true;
+        if(st1.size()==0) return true;
         return false;
     }
 };
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
