2d7c6a925eb55f08fe6bf47d8fb8e5e6e33b9a5c
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-12 11:01:39 +0530

    Time: 2071 ms (5.04%), Space: 138.5 MB (95.12%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,48 +2,49 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
         // method 1
-        //  vector<int>ans;
-        //  int n = nums.size();
-        //  for(int i = 0; i < n-k+1 ; i++){
-        //      int mx = INT_MIN;
-        //      for(int j = i ; j < i + k ; j++){
-        //          mx = max(mx,nums[j]);
-
+        // vector<int> ans;
+        // int n = nums.size();
+        // for (int i = 0; i < n - k + 1; i++) {
+        //     int mx = INT_MIN;
+        //     for (int j = i; j < i + k; j++) {
+        //         mx = max(mx, nums[j]);
         //     }
         //     ans.push_back(mx);
         // }
         // return ans;
+        
         // method 2
-        int n = arr.size();
-        int ngi[n];
-        stack<int> st;
-        st.push(n - 1);
-        ngi[n - 1] = n;
-        for (int i = n - 2; i >= 0; i--) {
-            while (st.size() > 0 && arr[st.top()] <= arr[i]) {
-                st.pop();
+            int n = arr.size();
+            int ngi[n];
+            stack<int> st;
+            st.push(n - 1);
+            ngi[n - 1] = n;
+            for (int i = n - 2; i >= 0; i--) {
+                while (st.size() > 0 && arr[st.top()] <= arr[i]) {
+                    st.pop();
+                }
+                if (st.size() == 0)
+                    ngi[i] = n;
+                else
+                    ngi[i] = st.top();
+                st.push(i);
             }
-            if (st.size() == 0)
-                ngi[i] = n;
-            else
-                ngi[i] = st.top();
-            st.push(i);
-        }
-        int j =0;
-        vector<int> ans;
-        for (int i = 0; i < n - k + 1; i++) {
-            int mx = arr[j];
-             j = i;
-            while (j < i + k) {
-                mx = arr[j];
-                if(ngi[j]>=i+k) break;
-                j = ngi[j];
+            int j =0;
+            vector<int> ans;
+            for (int i = 0; i < n - k + 1; i++) {
+                int mx = arr[j];
+                 j = i;
+                while (j < i + k) {
+                    if(j<i) j = i;
+                    mx = arr[j];
+                    if(ngi[j]>=i+k) break;
+                    j = ngi[j];
+                }
+                ans.push_back(mx);
             }
-            ans.push_back(mx);
-        }
         return ans;
-    }
-};
-// #include <print>
-// auto init = std::atexit(
-//     []() { std::println(std::fopen("display_runtime.txt", "w"), "0"); });
+         }
+    };
+    // #include <print>
+    // auto init = std::atexit(
+    //     []() { std::println(std::fopen("display_runtime.txt", "w"), "0"); });
