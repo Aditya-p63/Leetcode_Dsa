0782669a00551d8554b80b1210f85aacbd709ede
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-12 11:01:46 +0530

    Time: 36 ms (23.6%), Space: 139.1 MB (68.14%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,50 +1,19 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        // method 1
-        // vector<int> ans;
-        // int n = nums.size();
-        // for (int i = 0; i < n - k + 1; i++) {
-        //     int mx = INT_MIN;
-        //     for (int j = i; j < i + k; j++) {
-        //         mx = max(mx, nums[j]);
-        //     }
-        //     ans.push_back(mx);
-        // }
-        // return ans;
-        
-        // method 2
-            int n = arr.size();
-            int ngi[n];
-            stack<int> st;
-            st.push(n - 1);
-            ngi[n - 1] = n;
-            for (int i = n - 2; i >= 0; i--) {
-                while (st.size() > 0 && arr[st.top()] <= arr[i]) {
-                    st.pop();
-                }
-                if (st.size() == 0)
-                    ngi[i] = n;
-                else
-                    ngi[i] = st.top();
-                st.push(i);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        int n = nums.size();
+        deque<int>dq;
+        vector<int>ans;
+        for(int i = 0 ; i < n ; i++){
+            while(dq.size() > 0 && nums[i] > nums[dq.back()]){
+                dq.pop_back();
             }
-            int j =0;
-            vector<int> ans;
-            for (int i = 0; i < n - k + 1; i++) {
-                int mx = arr[j];
-                 j = i;
-                while (j < i + k) {
-                    if(j<i) j = i;
-                    mx = arr[j];
-                    if(ngi[j]>=i+k) break;
-                    j = ngi[j];
-                }
-                ans.push_back(mx);
-            }
+            dq.push_back(i);
+            int j = i-k+1;
+            while(dq.front()<j) dq.pop_front();
+            if( i >= k-1) ans.push_back(nums[dq.front()]);
+        }
         return ans;
-         }
-    };
-    // #include <print>
-    // auto init = std::atexit(
-    //     []() { std::println(std::fopen("display_runtime.txt", "w"), "0"); });
+    }
+};
