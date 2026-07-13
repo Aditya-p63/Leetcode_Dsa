1a0a3d059a585383a8edd2f235563804843edac8
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-13 18:19:46 +0530

    Time: 3 ms (46.13%), Space: 10 MB (11.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,23 +3,18 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         unordered_map<char,int>m1;//for s 
-        unordered_map<char,int>m2;//for t
         for(int i = 0 ; i < s.length() ; i++){
             m1[s[i]]++;
         }
-        for(int i = 0 ; i < t.length() ; i++){
-            m2[t[i]]++;
-        }
-
-        for(auto x : m1){
-            char ch = x.first;
-            int freq = x.second;
-            if(m2.find(ch)!=m2.end()){
-                int fq = m2[ch];
-                if(freq!=fq) return false;
+        for(int i = 0 ;i < t.size() ; i++){
+            char ch = t[i];
+            if(m1.find(ch)!=m1.end()){
+                m1[ch]--;
+                if(m1[ch]==0) m1.erase(ch);
             }
             else return false;
         }
+        if(m1.size()>0) return false;
         return true;
     }
 };
