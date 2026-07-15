7c17b99bab7304db3b46deebc5a2cb2dcbd2aa7d
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-15 16:54:53 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-int n , m;
-vector<vector<int>>grid;
-vector<vector<int>>dp;
-    int f(int i , int j ){
-        if(i==n-1 and j == m-1) return 1;
-        if(i < 0 || j<0 || i>=n || j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(grid[i][j]==1) return 0;
-        return dp[i][j]=f(i,j+1)+f(i+1,j);
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        grid = obstacleGrid;
-        n = grid.size();
-        m = grid[0].size();
-        dp.clear();
-        dp.resize(100,vector<int>(100,-1));
-        if(grid[n-1][m-1]==1) return 0;
-        return f(0,0); 
+    vector<vector<long long>> dp;
+
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (grid[n-1][m-1] == 1) return 0;
+        dp.assign(n, vector<long long>(m, 0));
+        dp[n-1][m-1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (grid[i][m-1] == 1) dp[i][m-1] = 0;
+            else dp[i][m-1] = dp[i+1][m-1];
+        }
+
+        for (int j = m - 2; j >= 0; j--) {
+            if (grid[n-1][j] == 1) dp[n-1][j] = 0;
+            else dp[n-1][j] = dp[n-1][j+1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = m - 2; j >= 0; j--) {
+                if (grid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
