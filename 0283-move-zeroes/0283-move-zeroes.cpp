7e1e3951e417b501ae5a285bc66c5e17b8aca6c1
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-16 18:51:38 +0530

    Time: 443 ms (5.13%), Space: 23.9 MB (56.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
+        //java mai fail kr jata
         for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                    if(nums[i]==0){
-                        int temp=nums[i];
-                        nums[i]=nums[j];
-                        nums[j]=temp;
+            for(int j=0;j<n-1-i;j++){
+                    if(nums[j]==0){
+                        int temp=nums[j];
+                        nums[j]=nums[j+1]; 
+                        nums[j+1]=temp;
                     }
             }
         }
