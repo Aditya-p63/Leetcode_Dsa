f502faccfb8233b918148eaba26e68d6c827bc73
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-18 08:47:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (28.44%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -7,14 +7,15 @@ public:
             ans = "0";
         } else {
             while (n > 0) {
-                ans = char((n % 2) + '0')            + ans; 
+                ans = char((n % 2) + '0') + ans;
                 n = n / 2;
             }
         }
         int p = ans.size();
-        for(int i = 0 ; i< p-1; i++){
-            if(ans[i]==ans[i+1]) return false;
+        for (int i = 0; i < p - 1; i++) {
+            if (ans[i] == ans[i + 1])
+                return false;
         }
-return true;
+        return true;
     }
 };
