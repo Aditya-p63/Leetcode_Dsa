1d359468854e108effca124ae86ec1dde1f6917d
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-18 08:44:08 +0530

    Time: 0 ms (100%), Space: 11.5 MB (71.99%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string predictPartyVictory(string s) {
+        int n = s.size();
+        queue<int> q;
+        queue<int> r;
+        queue<int> d;
+
+        for (int i = 0; i < n; i++) {
+            q.push(i);
+            if (s[i] == 'R') r.push(i);
+            else d.push(i);
+        }
+
+        while (q.size() > 1) {
+            if (s[q.front()] == 'X') {
+                q.pop();
+            }
+            else if (s[q.front()] == 'R') {
+                if (d.empty()) return "Radiant";
+                else {
+                    s[d.front()] = 'X';
+                    d.pop();
+                    q.push(q.front());
+                    q.pop();
+                    r.push(r.front());
+                    r.pop();
+                }
+            }
+            else {
+                if (r.empty()) return "Dire";
+                else {
+                    s[r.front()] = 'X';
+                    r.pop();
+                    q.push(q.front());
+                    q.pop();
+                    d.push(d.front());
+                    d.pop();
+                }
+            }
+        }
+
+        if (s[q.front()] == 'R') return "Radiant";
+        return "Dire";
+    }
+};
