e678bf21f36ab93e7cdc8bf473db8a039709e6b0
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-20 18:10:20 +0530

    Time: 0 ms (100%), Space: 16.6 MB (86.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
-        int fo = 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i = 0 ; i <n ; i++){
-            for(int j = 0 ; j < m ; j++ ){
-                if(grid[i][j]==1) fo++;
-                else if(grid[i][j]==2) q.push({i,j});
+        if (grid.empty())
+            return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        int days = 0, tot = 0, cnt = 0;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0)
+                    tot++;
+                if (grid[i][j] == 2)
+                    q.push({i, j});
             }
         }
-        q.push({-1,-1});
-        int min = 0;
-        vector<vector<int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.empty()){
-            auto cell  = q.front();
-            q.pop();
-            if(cell.first==-1 && cell.second==-1){
-                min++;
-                if(!q.empty()){
-                    q.push({-1,-1});
-                }
-                else break;
-            } 
-            else {
-                int i = cell.first;
-                int j = cell.second;
-                for(int d = 0 ; d < 4 ; d++){
-                    int nr = i+dir[d][0];
-                    int nc = j+dir[d][1];
-                    if(nr<0 || nc<0 || nr>= n || nc>= m) continue;
-                    if(grid[nr][nc]==2 || grid[nr][nc]==0) continue;
-                    fo--;
-                    grid[nr][nc]=2;
-                    q.push({nr,nc}); 
+        int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+        while (q.size() > 0) {
+            int k = q.size();
+            cnt += k;
+            while (k--) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
+                        grid[nx][ny] != 1)
+                        continue;
+                    grid[nx][ny] = 2;
+                    q.push({nx, ny});
                 }
             }
+            if (!q.empty())
+                days++;
         }
-        return (fo==0) ? min-1 : -1;
+        return tot == cnt ? days : -1;
     }
 };
