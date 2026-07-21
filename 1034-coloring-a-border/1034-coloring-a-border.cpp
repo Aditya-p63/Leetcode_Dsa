3b45c799f1f093cc8a534a04f1784fc8afbf3eb5
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-21 18:50:55 +0530

    Time: 3 ms (31.51%), Space: 17.8 MB (86.85%) - LeetHub

---
+++ 1034-coloring-a-border/1034-coloring-a-border.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<pair<int, int>> result;
+    void dfs(vector<vector<int>>& grid, int r, int c, int newcolor , int oc) {
+        if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() ||
+            grid[r][c] != oc)
+            return;
+        grid[r][c] = -newcolor;
+        dfs(grid, r + 1, c, newcolor,oc);
+        dfs(grid, r - 1, c, newcolor,oc);
+        dfs(grid, r, c + 1, newcolor,oc);
+        dfs(grid, r, c - 1, newcolor,oc);
+        if (!(r == 0 || c == 0 || r == grid.size() - 1 || c == grid[0].size() - 1 ||
+            grid[r + 1][c] != -newcolor || grid[r - 1][c] != -newcolor ||
+            grid[r][c - 1] != -newcolor || grid[r][c + 1] != -newcolor)) {
+            result.push_back({r, c});
+        }
+    }
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col,
+                                    int color) {
+        int oc = grid[row][col];
+        if(oc==color) return grid;
+        dfs(grid, row, col, color,oc);
+        for (auto p : result) {
+            int i = p.first;
+            int j = p.second;
+            grid[i][j]=oc;
+        }
+
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                if(grid[i][j]<0){
+                    grid[i][j]=-1*grid[i][j];
+                }
+            }
+        }
+        return grid;
+    }
+};
