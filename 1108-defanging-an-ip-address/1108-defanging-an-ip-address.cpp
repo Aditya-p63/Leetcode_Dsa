e58064d9c1f67be9579ca9bb7f968e1518b69870
Aditya raj prasad <prasad987aditya@gmail.com>
2026-07-21 18:55:08 +0530

    Time: 2 ms (55.81%), Space: 7.8 MB (49.26%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string s) {
+        string ans = "";
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i]=='.'){
+                ans+="[.]";
+            }
+            else ans+=s[i];
+        }
+        return ans;
+    }
+};
