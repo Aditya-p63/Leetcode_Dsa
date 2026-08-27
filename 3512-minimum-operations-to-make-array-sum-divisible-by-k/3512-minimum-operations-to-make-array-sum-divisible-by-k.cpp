b89d486435549a7a0cdc30c9463a569437c4a1b0
Aditya raj prasad <prasad987aditya@gmail.com>
2026-08-27 21:18:43 +0530

    Time: 0 ms (100%), Space: 45.1 MB (97.41%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -6,9 +6,6 @@ public:
         for(int i  = 0 ; i <  n ; i++){
             sum+=nums[i];
         }
-        sum = sum%k;
-        if(sum%k==0) return 0;
-        else if(sum<k) return sum;
-        return -1;
+        return sum%k;
     }
 };
