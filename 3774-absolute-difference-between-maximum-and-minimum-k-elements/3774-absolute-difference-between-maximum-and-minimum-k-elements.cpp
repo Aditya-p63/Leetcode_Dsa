651264031fc2dfd699cb132798d891266631a4c1
Aditya raj prasad <prasad987aditya@gmail.com>
2026-08-27 21:21:00 +0530

    Time: 0 ms (100%), Space: 33.3 MB (5.54%) - LeetHub

--- 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.cpp
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int absDifference(vector<int>& nums, int k) {
+        priority_queue<int>large;
+        priority_queue<int,vector<int>,greater<int>>small;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int ls = 0 , ss = 0;
-        for(int i = 0 ; i < k ; i++){
-            ls += nums[i];
-            ss += nums[n-i-1];
-        }   
+        for(int i = 0 ; i < n ; i++){
+            large.push(nums[i]);
+        }
+        for(int i = 0 ; i < n ; i++){
+            small.push(nums[i]);
+        }
+        int ls= 0;
+        int ss= 0;
+        int j = k;
+        while(k--){
+            ls+=large.top();
+            large.pop();
+        }
+        
+        while(j--){
+            ss+=small.top();
+            small.pop();
+        }
         return abs(ls-ss);
+        
+
     }
 };
