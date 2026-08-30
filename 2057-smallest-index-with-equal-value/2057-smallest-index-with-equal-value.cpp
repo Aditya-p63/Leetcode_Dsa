2e6bebcfd20cdad888edccd6afe09c62e6ed3566
Aditya raj prasad <prasad987aditya@gmail.com>
2026-08-30 21:43:47 +0530

    Time: 0 ms (100%), Space: 25.8 MB (96.8%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int smallestEqual(vector<int>& nums) {
-        int n=nums.size();
-        int x=0;
-        int y=-1;
-        bool flag=false;
-        for(int i=0;i<n;i++){
-            if(x%10==nums[i]){
-                y=i;
-                flag=true;
-                break;
+        int n = nums.size();
+        // int x=0;
+        // int y=-1;
+        // bool flag=false;
+        // for(int i=0;i<n;i++){
+        //     if(x%10==nums[i]){
+        //         y=i;
+        //         flag=true;
+        //         break;
+        //     }
+        //     x++;
+        // }
+        // if(flag==true) return y;
+        // return -1;
+        for (int i = 0; i < n; i++) {
+            if (i % 10 == nums[i]) {
+                return i;
             }
-            x++;
         }
-        if(flag==true) return y;
         return -1;
     }
 };
